a28b335aaa2bc03c0e0267cface15576db1a51df
Ryan Hubble <rhubble1987@gmail.com>
2020-10-31 22:43:30 -0400

    Added the rest of the logic for generating the password

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener ("click", function() {
+function generatePassword() {
   var requestedLength = prompt("Specify the character length of the password (must be between 8 and 128 characters)");
   while (requestedLength < 8 || requestedLength > 128) {
     if (requestedLength < 8) {
@@ -10,7 +10,11 @@ generateBtn.addEventListener ("click", function() {
     } else if (requestedLength > 128) {
       alert("Cannot exceed 128 characters in length.");
       var requestedLength = prompt("Specify the character length of the password (must be between 8 and 128 characters)");
-    } 
+    } else if (requestedLength !== Number); {
+      alert("You must enter a number.");
+      var requestedLength = prompt("Specify the character length of the password (must be between 8 and 128 characters)");
+    }
+
 }
 
 
@@ -19,11 +23,37 @@ var requestedLowercase = confirm("Should the password contain lower case charact
 var requestedNumbers = confirm("Should the password contain numbers? Click 'Ok' if yes, or 'Cancel' if no.");
 var requestedSpecialCharacters = confirm("Should the password contain special characters? Click 'Ok' if yes, or 'Cancel' if no.");
 
+//This code will validate that the user selected at least one of the character criteria.
+
+while (requestedUppercase !== true && requestedLowercase !== true && requestedNumbers !== true && requestedSpecialCharacters !== true) {
+  alert("You must select at least one type of character.")
+  var requestedUppercase = confirm("Should the password contain upper case letters? Click 'Ok' if yes, or 'Cancel' if no.");
+  var requestedLowercase = confirm("Should the password contain lower case characters? Click 'Ok' if yes, or 'Cancel' if no.");
+  var requestedNumbers = confirm("Should the password contain numbers? Click 'Ok' if yes, or 'Cancel' if no.");
+  var requestedSpecialCharacters = confirm("Should the password contain special characters? Click 'Ok' if yes, or 'Cancel' if no.");
+}
+
 console.log(requestedLength);
 console.log(requestedUppercase);
 console.log(requestedLowercase);
 console.log(requestedNumbers);
 console.log(requestedSpecialCharacters);
+
+var selectedCriteria = 0;
+
+if (requestedLength) {
+  selectedCriteria = selectedCriteria++;
+}
+
+if (requestedUppercase) {
+  selectedCriteria = selectedCriteria++;
+}
+
+if (requestedLowercase) {
+  selectedCriteria = selectedCriteria++
+}
+
+
 
 
 var password = "";
@@ -42,7 +72,7 @@ console.log(alphabeticCharacters);
 
   function addLowercaseCharacters() {
      
-      return password = password.replace(password.charAt(Math.floor(Math.random() * password.length + 1)), alphabeticCharacters.charAt(Math.floor(Math.random() * 26)));
+      return password = password.replace(password.charAt(Math.floor(Math.random() * password.length)), alphabeticCharacters.charAt(Math.floor(Math.random() * 26)));
 
 }
 
@@ -60,7 +90,7 @@ console.log(password);
 //This will then add any uppercase letters
 function addUppercaseCharacters() {
      
-  return password = password.replace(password.charAt(Math.floor(Math.random() * password.length + 1)),  alphabeticCharacters.charAt(Math.floor(Math.random() * 26)).toUpperCase());
+  return password = password.replace(password.charAt(Math.floor(Math.random() * password.length)),  alphabeticCharacters.charAt(Math.floor(Math.random() * 26)).toUpperCase());
 
 }
 
@@ -72,10 +102,138 @@ if (requestedUppercase) {
 
 console.log(password);
 
+if (requestedLowercase === false) {
+  password.toUpperCase();
+}
+
+console.log(password);
+
+
+//This code will add some special characters
+var specialCharacters = "!@#$%^&*()_-+=?";
+function addSpecialCharacters(){
+  return password = password.replace(password.charAt(Math.floor(Math.random() * password.length)), specialCharacters.charAt(Math.floor(Math.random() * 15)));
+}
+
+if (requestedSpecialCharacters) {
+  for (i = 0; i < (requestedLength / 6); i++) {
+    addSpecialCharacters();
+}
+
+console.log(password);
+
+
+/*
+
+This next code should remove all numeric characters if the user selected no for including numeric characters.
+After clearing out the password, it will then use a for loop to generate a string of alphabetic characters based on the requested
+length. And then, depending on what the user said for upper and lower case characters, it will add them accordingly with some
+conditionals.
+
+*/
+
+
+function removeNumbers() {
+  return password = password.replace(password,"");
+}
+
+function replaceWithALetter() {
+  return password = alphabeticCharacters.charAt(Math.floor(Math.random() * 26))
+}
+
+function addMoreLetters() {
+  return password = password + alphabeticCharacters.charAt(Math.floor(Math.random() * 26));
+}
+
+function replaceWithASpecialCharacter() {
+  return password = specialCharacters.charAt(Math.floor(Math.random() * 15))
+}
+
+function addMoreSpecialCharacters() {
+  return password = password + specialCharacters.charAt(Math.floor(Math.random() * 15));
+}
 
 
 
-});
+if (requestedNumbers === false && requestedUppercase === true && requestedLowercase === true && requestedSpecialCharacters === true) {
+  removeNumbers();
+  replaceWithALetter();
+  for (i = 0; i < (requestedLength - 1); i++) {
+    addMoreLetters();
+  }
+  for (i = 0; i < (requestedLength / 5); i++) {
+      addUppercaseCharacters();
+    }
+  for (i = 0; i < (requestedLength / 5); i++) {
+      addSpecialCharacters();
+    }
+
+  }
+
+  if (requestedNumbers === false && requestedUppercase === true && requestedLowercase === false && requestedSpecialCharacters === false) {
+    removeNumbers();
+  replaceWithALetter();
+  for (i = 0; i < (requestedLength - 1); i++) {
+    addMoreLetters();
+  }
+    password.toUpperCase();
+  }
+
+  if (requestedNumbers === false && requestedUppercase === false && requestedLowercase === true && requestedSpecialCharacters === true) {
+    removeNumbers();
+  replaceWithALetter();
+  for (i = 0; i < (requestedLength - 1); i++) {
+    addMoreLetters();
+  }
+  for (i = 0; i < (requestedLength / 6); i++) {
+    addSpecialCharacters();
+}
+  }
+  if (requestedNumbers === false && requestedUppercase === false && requestedLowercase === true && requestedSpecialCharacters === false) {
+    removeNumbers();
+  replaceWithALetter();
+  for (i = 0; i < (requestedLength - 1); i++) {
+    addMoreLetters();
+  }
+
+}  
+if (requestedNumbers === false && requestedUppercase === false && requestedLowercase === false && requestedSpecialCharacters === true) {
+  removeNumbers();
+  replaceWithASpecialCharacter();
+for (i = 0; i < (requestedLength - 1); i++) {
+  addMoreSpecialCharacters();
+}
+
+}  
+if (requestedNumbers === false && requestedUppercase === true && requestedLowercase === true && requestedSpecialCharacters === false) {
+  removeNumbers();
+replaceWithALetter();
+for (i = 0; i < (requestedLength - 1); i++) {
+  addMoreLetters();
+}
+for (i = 0; i < (requestedLength / 5); i++) {
+  addUppercaseCharacters();
+}
+}
+
+
+if (requestedNumbers === false && requestedUppercase === true && requestedLowercase === false && requestedSpecialCharacters === true) 
+emoveNumbers();
+replaceWithALetter();
+for (i = 0; i < (requestedLength - 1); i++) {
+  addMoreLetters();
+}
+password.toUpperCase();
+for (i = 0; i < (requestedLength / 6); i++) {
+  addSpecialCharacters();
+}
+}
+
+
+
+console.log(password);
+
+}
 
 
 
@@ -130,3 +288,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  passwordText.textContent = passwordText;
